96770e595d8f72f53c415f0b069a026e9a00aacb
Keshok Sivalingam <keshok@live.ca>
2019-02-13 20:52:35 -0500

    Updated PlayerController script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,13 +10,16 @@ public class PlayerController : MonoBehaviour
 
     // PRIVATE VARIABLES
     private Rigidbody2D rBody;
-    private bool canJump = false;
+    //private bool canJump = false;
+    private Animator anim;
+    private bool isFacingRight = true;
 
     // Reserved function. Runs only once when the object is created.
     // Used for initialization
     void Start()
     {
         rBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
@@ -24,7 +27,7 @@ public class PlayerController : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Space)) // Listens to my space bar key being pressed
         {
             rBody.AddForce(new Vector2(0, jumpForce));
-            canJump = false;
+            //canJump = false;
         }
     }
 
@@ -37,5 +40,26 @@ public class PlayerController : MonoBehaviour
         float horiz = Input.GetAxis("Horizontal");
 
         rBody.velocity = new Vector2(horiz * speed, rBody.velocity.y);
+
+        // Check direction of the player
+        if(horiz < 0 && isFacingRight)
+        {
+            Flip();
+        }
+        else if(horiz > 0 && !isFacingRight)
+        {
+            Flip();
+        }
+
+        // Update Animator Information
+        anim.SetFloat("Speed", Mathf.Abs(horiz));
+    }
+
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector2 temp = transform.localScale;
+        temp.x *= -1;
+        transform.localScale = temp;
     }
 }
